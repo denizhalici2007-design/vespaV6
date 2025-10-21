4b527ee58d7883f7674e032526ae2223c115682e
DenizHalici <denizhalici2007@gmail.com>
2025-10-21 17:25:53 +0300

    vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,16 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: ".",
-
-  server: {
-    host: true,
-    port: Number(process.env.PORT) || 5173,
-    open: true,
-  },
-
-  preview: {
-    allowedHosts: ['vespav5.onrender.com'],
-    host: true,
-    port: Number(process.env.PORT) || 5173,
+  build: {
+    outDir: "dist",
   },
 });
